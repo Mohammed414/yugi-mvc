4996869673c8ee5a782ffd624954207670dc583d
Mohammed Mahmood <kaifodo414@gmail.com>
2022-09-30 01:09:46 +0300

    chore(bootstrap.php): create an autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,10 +3,17 @@
 require_once 'config/config.php';
 
 
-// Load all libraries 
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+// // Load all libraries 
+// require_once 'libraries/Core.php';
+// require_once 'libraries/Controller.php';
+// require_once 'libraries/Database.php';
 
+// AUTOLOADER FOR ALL CORE LIBRARIES
+// Every new library added will be auto loaded here 
+spl_autoload_register(
+    function($className) {
+        require_once 'libraries/'. $className .'.php';
+    }
+)
 
 ?>
